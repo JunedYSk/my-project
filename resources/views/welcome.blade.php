adaf563a0d085cd10a17761406be213aaba4f431
JunedYSk <151511237+JunedYSk@users.noreply.github.com>
2024-07-23 14:19:29 +0530

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+  @vite('resources/js/app.js')
+  <title>Document</title>
+</head>
+
+<body>
+  <h1 class="text-3xl font-bold text-red-400 p-8">
+    Laravel Project
+  </h1>
+</body>
+
+</html>
